fa7cb474b65108b4b42cf6b340452c106fa418a0
luishector06 <53093130+luishector06@users.noreply.github.com>
2019-07-19 12:26:38 -0600

    Commit Inicial

---
+++ src/dummy/HelloWorld.java
@@ -0,0 +1,22 @@
+package dummy;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	    HelloWorld helloWorld = new HelloWorld();
+	    helloWorld.go();
+	    
+	}
+
+	public void go(){
+		
+		String respuesta = JOptionPane.showInputDialog(null, "HelloWorld");
+		JOptionPane.showConfirmDialog(null, respuesta, "Gracias", JOptionPane.YES_NO_CANCEL_OPTION);
+	   	
+	}
+	
+}
